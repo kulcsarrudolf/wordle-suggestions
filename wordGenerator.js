103c161ff50ea2aece86996d38187fc8a293364d
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2022-02-17 10:37:02 +0200

    Installed check-word

--- wordGenerator.js
+++ wordGenerator.js
@@ -1,4 +1,12 @@
+import checkWord from "check-word";
+
 export const getAllNLetterWords = (numberOfLetters) => {
+    const a = checkWord("en");
+
+    console.log(a.check("dog")); // true
+    console.log(a.check("perro")); // false
+    console.log(a.check("hi")); // true
+
     const words = ["apple", "mappx", "wzaaaa", "boy"];
 
     const validWords = words.filter((word) => word.length > 4);
