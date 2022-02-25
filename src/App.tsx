46f5c8dc0828da5f815e7ed42ba819f443316525
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2022-02-25 08:02:33 +0200

    MUI migration preparation

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
-const App = () => <>Wordle Suggestions</>;
+import React from 'react';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme();
+
+const App = () => <ThemeProvider theme={theme}>Wordle Suggestions</ThemeProvider>;
 
 export default App;
