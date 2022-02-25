dcbae970db51bdd0f9c2848c7865927d82d2e018
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2022-02-25 09:56:29 +0200

    Good Letter implementation started

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -2,14 +2,23 @@ import React, { useState } from 'react';
 import { TextField } from '@mui/material';
 
 const Main = () => {
-  const [goodLetters, setGoodLetters] = useState([]);
+  const [goodLetters, setGoodLetters] = useState<Array<string>>([]);
   const [badLetters, setBadLetters] = useState([]);
   const [placedLetters, setPlacedLetters] = useState([]);
   const [yellowLetters, setYellowLetters] = useState([]);
 
   return (
     <>
-      <TextField label="Outlined" variant="outlined" />
+      <TextField
+        label="Good Letters"
+        variant="outlined"
+        value={goodLetters.join('')}
+        onChange={(e) => {
+          let x = Array.from(e.target.value);
+          x = x.map((letter) => letter.toUpperCase());
+          setGoodLetters([...new Set(x)]);
+        }}
+      />
     </>
   );
 };
