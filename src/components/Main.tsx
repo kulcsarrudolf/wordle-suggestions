5afba284726cd1dd0f27259d0ed1f60fd67cfb22
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2022-02-26 07:33:41 +0200

    Suggestions Displayed

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { TextField } from '@mui/material';
+
+import { getWordleSuggestions } from '../service/wordle-suggestions';
 
 const Main = () => {
   const [goodLetters, setGoodLetters] = useState<Array<string>>([]);
-  const [badLetters, setBadLetters] = useState([]);
+  const [badLetters, setBadLetters] = useState<Array<string>>([]);
   const [placedLetters, setPlacedLetters] = useState([]);
   const [yellowLetters, setYellowLetters] = useState([]);
+
+  const [suggestions, setSuggestions] = useState<Array<string>>([]);
+
+  const lettersToArray = (lettersString: string) => {
+    let x = Array.from(lettersString);
+    x = x.map((letter) => letter.toUpperCase());
+    return [...new Set(x)];
+  };
+
+  useEffect(() => {
+    setSuggestions(getWordleSuggestions(goodLetters, badLetters, [], []));
+
+    // setSuggestions(getWordleSuggestions([], [], [], []));
+  }, [goodLetters, badLetters]);
 
   return (
     <>
@@ -14,11 +30,22 @@ const Main = () => {
         variant="outlined"
         value={goodLetters.join('')}
         onChange={(e) => {
-          let x = Array.from(e.target.value);
-          x = x.map((letter) => letter.toUpperCase());
-          setGoodLetters([...new Set(x)]);
+          setGoodLetters(lettersToArray(e.target.value));
         }}
       />
+
+      <TextField
+        label="Bad Letters"
+        variant="outlined"
+        value={badLetters.join('')}
+        onChange={(e) => {
+          setBadLetters(lettersToArray(e.target.value));
+        }}
+      />
+
+      {suggestions.map((word) => (
+        <>{word}</>
+      ))}
     </>
   );
 };
