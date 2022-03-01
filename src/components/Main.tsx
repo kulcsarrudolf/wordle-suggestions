6c368582eae0e6d488187d53c3be0be47e511e88
Kulcsar Rudolf <kulcsarrudolf@gmail.com>
2022-03-01 08:49:15 +0200

    Placed Letters filter added

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,12 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { TextField } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { TextField, Grid, Chip, Stack, Typography } from '@mui/material';
 
 import { getWordleSuggestions } from '../service/wordle-suggestions';
 
 const Main = () => {
   const [goodLetters, setGoodLetters] = useState<Array<string>>([]);
   const [badLetters, setBadLetters] = useState<Array<string>>([]);
-  const [placedLetters, setPlacedLetters] = useState([]);
+  const [placedLetters, setPlacedLetters] = useState<any>(
+    new Map([
+      ['FIRST_LETTER', ''],
+      ['SECOND_LETTER', ''],
+      ['THIRD_LETTER', ''],
+      ['FOURTH_LETTER', ''],
+      ['FIFTH_LETTER', ''],
+    ])
+  );
+
   const [yellowLetters, setYellowLetters] = useState([]);
 
   const [suggestions, setSuggestions] = useState<Array<string>>([]);
@@ -18,34 +27,98 @@ const Main = () => {
   };
 
   useEffect(() => {
-    setSuggestions(getWordleSuggestions(goodLetters, badLetters, [], []));
+    const a: Array<any> = [];
+
+    if (placedLetters.get('FIRST_LETTER') !== '') {
+      a.push([placedLetters.get('FIRST_LETTER'), 1]);
+    }
+
+    if (placedLetters.get('SECOND_LETTER') !== '') {
+      a.push([placedLetters.get('SECOND_LETTER'), 2]);
+    }
+
+    if (placedLetters.get('THIRD_LETTER') !== '') {
+      a.push([placedLetters.get('THIRD_LETTER'), 3]);
+    }
+
+    if (placedLetters.get('FOURTH_LETTER') !== '') {
+      a.push([placedLetters.get('FOURTH_LETTER'), 4]);
+    }
+
+    if (placedLetters.get('FIFTH_LETTER') !== '') {
+      a.push([placedLetters.get('FIFTH_LETTER'), 5]);
+    }
+
+    setSuggestions(getWordleSuggestions(goodLetters, badLetters, a, []));
 
     // setSuggestions(getWordleSuggestions([], [], [], []));
-  }, [goodLetters, badLetters]);
+    console.log(placedLetters);
+  }, [goodLetters, badLetters, placedLetters]);
 
   return (
     <>
-      <TextField
-        label="Good Letters"
-        variant="outlined"
-        value={goodLetters.join('')}
-        onChange={(e) => {
-          setGoodLetters(lettersToArray(e.target.value));
-        }}
-      />
+      <Grid container direction="column" justifyContent="center" alignItems="center" spacing={2}>
+        <Grid item>
+          <TextField
+            label="Good Letters"
+            variant="outlined"
+            value={goodLetters.join('')}
+            onChange={(e) => {
+              setGoodLetters(lettersToArray(e.target.value));
+            }}
+          />
+        </Grid>
 
-      <TextField
-        label="Bad Letters"
-        variant="outlined"
-        value={badLetters.join('')}
-        onChange={(e) => {
-          setBadLetters(lettersToArray(e.target.value));
-        }}
-      />
+        <Grid item>
+          <TextField
+            label="Bad Letters"
+            variant="outlined"
+            value={badLetters.join('')}
+            onChange={(e) => {
+              setBadLetters(lettersToArray(e.target.value));
+            }}
+          />
+        </Grid>
 
-      {suggestions.map((word) => (
-        <>{word}</>
-      ))}
+        <Grid item>
+          {Array.from(placedLetters.keys()).map((key: any) => (
+            <>
+              <TextField
+                variant="outlined"
+                value={placedLetters.get(key)}
+                inputProps={{ min: 0, style: { textAlign: 'center', textTransform: 'uppercase' } }}
+                style={{ width: '5rem', margin: '0.25rem' }}
+                onChange={(e) => {
+                  console.log(key);
+                  console.log(e.target.value);
+                  const placedLettersCopy = placedLetters;
+                  placedLettersCopy.set(key, e.target.value);
+
+                  console.log(placedLettersCopy);
+                  setPlacedLetters(new Map(placedLettersCopy));
+                }}
+              />
+            </>
+          ))}
+        </Grid>
+
+        <Grid item>
+          <Typography>{suggestions.length}</Typography>
+        </Grid>
+        {suggestions.length < 1000 && (
+          <Grid item>
+            {suggestions.map((word) => (
+              <Chip
+                variant="outlined"
+                color="primary"
+                size="small"
+                label={word}
+                style={{ margin: '0.25rem' }}
+              />
+            ))}
+          </Grid>
+        )}
+      </Grid>
     </>
   );
 };
